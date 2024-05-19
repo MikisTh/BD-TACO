4f1edda9eaab21a38aaf928091f97b7a675d1496
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-05-19 18:15:44 -0300

    Create CreateTable_DRI_Lactentes.sql
    Add

---
+++ Taco_BD/API_taco/CreateTable_DRI_Lactentes.sql
@@ -0,0 +1,33 @@
+.import lactantes.csv lactantes
+
+CREATE TABLE RecomendacaoNutricional (
+    'IdRDA' varchar(20),
+    'IdDieta' varchar(20),
+    'EnergiaKcal' varchar(20),
+    'EnergiaKj' varchar(20),
+    'Proteina' varchar(20),
+    'Lipideos' varchar(20),
+    'Colesterol' varchar(20),
+    'Carboidrato' varchar(20),
+    'FibraAlimentar' varchar(20),
+    'Cinzas' varchar(20),
+    'Calcio' varchar(20),
+    'Magnesio' varchar(20),
+    'Manganes' varchar(20),
+    'Fosforo' varchar(20),
+    'Ferro' varchar(20),
+    'Sodio' varchar(20),
+    'Postassio' varchar(20),
+    'Cobre' varchar(20),
+    'Zinco' varchar(20),
+    'Retinol' varchar(20),
+    're' varchar(20),
+    'rae' varchar(20),
+    'Tiamina' varchar(20),
+    'Riboflavina' varchar(20),
+    'Piridoxina' varchar(20),
+    'Niacina' varchar(20),
+    'AcidoAscorbico' varchar(20),    
+    FOREIGN KEY (Id_RDA) REFERENCES RDA (Id_RDA)
+
+);
