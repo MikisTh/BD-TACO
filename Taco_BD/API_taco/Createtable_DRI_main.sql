31c1169356f50341d0d4e366b61bce242f97563e
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-05-22 09:32:08 -0300

    Update Createtable_DRI_main.sql
    Add

--- Taco_BD/API_taco/Createtable_DRI_main.sql
+++ Taco_BD/API_taco/Createtable_DRI_main.sql
@@ -168,3 +168,37 @@ CREATE TABLE RDA_Idosos (
     CONSTRAINT fk_PesDieta FOREIGN KEY (Id_Dieta) REFERENCES Dieta (Id_Dieta)
 
 );
+
+CREATE TABLE RDA_Lactantes (
+    'IdLactantes' int PRIMARY KEY AUTOINCREMENT,
+    'Id_RDA' int NOT NULL FOREIGN KEY,
+    'Id_Dieta' int NOT NULL FOREIGN KEY,
+    'EnergiaKcal' varchar(20),
+    'EnergiaKj' varchar(20),
+    'Proteina' varchar(20),
+    'Lipideos' varchar(20),
+    'Colesterol' varchar(20),
+    'Carboidrato' varchar(20),
+    'FibraAlimentar' varchar(20),
+    'Cinzas' varchar(20),
+    'Calcio' varchar(20),
+    'Magnesio' varchar(20),
+    'Manganes' varchar(20),
+    'Fosforo' varchar(20),
+    'Ferro' varchar(20),
+    'Sodio' varchar(20),
+    'Postassio' varchar(20),
+    'Cobre' varchar(20),
+    'Zinco' varchar(20),
+    'Retinol' varchar(20),
+    're' varchar(20),
+    'rae' varchar(20),
+    'Tiamina' varchar(20),
+    'Riboflavina' varchar(20),
+    'Piridoxina' varchar(20),
+    'Niacina' varchar(20),
+    'AcidoAscorbico' varchar(20),    
+    CONSTRAINT fk_PesRDA FOREIGN KEY (Id_RDA) REFERENCES RDA (Id_RDA),
+    CONSTRAINT fk_PesDieta FOREIGN KEY (Id_Dieta) REFERENCES Dieta (Id_Dieta)
+
+);
