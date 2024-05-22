a5fc616e4cca40d86b7bbfad5aab9b2dd9011fdd
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-05-22 09:07:05 -0300

    Update CreateTable_DRI_Adultos.sql
    Add

--- Taco_BD/API_taco/CreateTable_DRI_Adultos.sql
+++ Taco_BD/API_taco/CreateTable_DRI_Adultos.sql
@@ -1,9 +1,9 @@
 .import adultos.csv adultos
 
 CREATE TABLE RecomendacaoNutricional (
-    'IdAdultos' integer PRIMARY KEY AUTOINCREMENT,
-    'IdRDA' integer,
-    'IdDieta' integer,
+    'IdAdultos' int PRIMARY KEY AUTOINCREMENT,
+    'IdRDA' int,
+    'IdDieta' int,
     'EnergiaKcal' varchar(20),
     'EnergiaKj' varchar(20),
     'Proteina' varchar(20),
